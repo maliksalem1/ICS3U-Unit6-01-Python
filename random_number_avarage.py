2328ec92baea086d1e9aa6bbd13c570461650985
Malik Salem <malik.salem@mths.ca>
2023-01-06 22:33:05 +0000

    code

---
+++ random_number_avarage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Created by: maliksalem1
+# Created on: Dec 2022
+# This program generates 10 random numbers and finds the average
+
+import random
+
+
+def main():
+    # this function uses an array
+
+    random_numbers = []
+
+    for loop_counter in range(0, 10):
+        single_random_number = random.randint(0, 100)
+        random_numbers.append(single_random_number)
+        print("The random number is: {0}".format(random_numbers[loop_counter]))
+
+    # sum(random_numbers) adds all variables in the array
+    average = sum(random_numbers) / len(random_numbers)
+    print("")
+    print("The average is {0}".format(average))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
